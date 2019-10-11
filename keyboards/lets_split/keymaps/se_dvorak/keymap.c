ab0d68a73d917142e38ab90727fa823ec9f45f75
MGrass1 <marcus.grass@gmail.com>
2019-10-11 17:09:14 +0200

    fixed some porting errors

--- keyboards/lets_split/keymaps/se_dvorak/keymap.c
+++ keyboards/lets_split/keymaps/se_dvorak/keymap.c
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_DVORAK] = LAYOUT_ortho_4x12(
         KC_TAB,  SFTAPSTR, COMLBR,   DOTRBR,  KC_P,  KC_Y,   KC_F,  KC_G,  KC_C,    KC_R,    KC_L,    KC_BSPC,
-ESC_CTL, KC_A,     KC_O,     KC_E,    KC_U,  KC_I,   KC_D,  KC_H,  KC_T,    KC_N,    KC_S,    KC_ENT,
+        ESC_CTL, KC_A,     KC_O,     KC_E,    KC_U,  KC_I,   KC_D,  KC_H,  KC_T,    KC_N,    KC_S,    KC_ENT,
         KC_LSFT, SCOLCOL,  KC_Q,     KC_J,    KC_K,  KC_X,   KC_B,  KC_M,  KC_W,    KC_V,    KC_Z,    KC_LSFT,
-        KC_LCTL, KC_LGUI,  KC_LALT,  WILD,    LOWER,     KC_SPC,    NUM ,  RAISE,   KC_ALGR, SETTS,   KC_RCTL
+        KC_LCTL, KC_LGUI,  KC_LALT,  WILD,    LOWER, KC_SPC, KC_SPC,NUM ,  RAISE,   KC_ALGR, SETTS,   KC_RCTL
 ),
 
 /* Dvorak ANSI
@@ -73,9 +73,9 @@ ESC_CTL, KC_A,     KC_O,     KC_E,    KC_U,  KC_I,   KC_D,  KC_H,  KC_T,    KC_N
  */
 [_DVORAK_ANSI] = LAYOUT_ortho_4x12(
         KC_TAB,  KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,  KC_Y,   KC_F,  KC_G,  KC_C,    KC_R,    KC_L,    KC_BSPC,
-ESC_CTL, KC_A,     KC_O,     KC_E,    KC_U,  KC_I,   KC_D,  KC_H,  KC_T,    KC_N,    KC_S,    KC_ENT,
+        ESC_CTL, KC_A,     KC_O,     KC_E,    KC_U,  KC_I,   KC_D,  KC_H,  KC_T,    KC_N,    KC_S,    KC_ENT,
         KC_LSFT, KC_SCLN,  KC_Q,     KC_J,    KC_K,  KC_X,   KC_B,  KC_M,  KC_W,    KC_V,    KC_Z,    KC_LSFT,
-        KC_LCTL, KC_LGUI,  KC_LALT,  WILD,    LOWANS,    KC_SPC,    NUM ,  RAISE,   KC_ALGR, SETTS,   KC_RCTL
+        KC_LCTL, KC_LGUI,  KC_LALT,  WILD,    LOWANS,KC_SPC, KC_SPC,NUM,   RAISE,   KC_ALGR, SETTS,   KC_RCTL
 ),
 
 /* QWERTY ANSI
@@ -91,9 +91,9 @@ ESC_CTL, KC_A,     KC_O,     KC_E,    KC_U,  KC_I,   KC_D,  KC_H,  KC_T,    KC_N
  */
 [_QWERTY_ANSI] = LAYOUT_ortho_4x12(
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,  KC_T,   KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,     KC_X,     KC_C,    KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_QUOT, KC_LSFT,
-        KC_LCTL, KC_LGUI,  KC_LALT,  WILD,    LOWANS,    KC_SPC,    NUM ,  RAISE,   KC_ALGR, SETTS,   KC_RCTL
+        KC_LCTL, KC_LGUI,  KC_LALT,  WILD,    LOWANS,KC_SPC, KC_SPC,NUM,   RAISE,   KC_ALGR, SETTS,   KC_RCTL
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -110,7 +110,7 @@ ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L
         KC_TRNS, KC_EXLM, NO_AT  , KC_HASH, NO_DLR , KC_PERC, NO_CIRC, NO_AMPR, NO_LPRN, NO_RPRN, NO_ASTR, KC_TRNS,
         KC_TRNS, NO_PLUS, NO_EQL , NO_LBRC, NO_RBRC, NO_QUES, NO_BSLS, NO_SLSH, NO_LCBR, NO_RCBR, NO_MINS, KC_TRNS,
         KC_TRNS, KC_TRNS, _CUT   , _COPY  , _PASTE , NO_TILD, NO_GRV , NO_PIPE, NO_AA  , NO_AE  , NO_OSLH, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Lower ANSI
@@ -128,7 +128,7 @@ ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L
         KC_TRNS, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_TRNS,
         KC_TRNS, KC_PLUS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUES, KC_BSLS, KC_SLSH, KC_LCBR, KC_RCBR, KC_MINS, KC_TRNS,
         KC_TRNS, KC_TRNS, _CUT   , _COPY  , _PASTE , KC_TILD, KC_GRV , KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Raise
@@ -146,7 +146,7 @@ ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L
         KC_TRNS, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_TRNS,
         KC_TRNS, KC_LEFT, KC_RGHT, KC_UP  , KC_DOWN, KC_F11,  KC_F12 , KC_PSCR, KC_PGUP, KC_HOME, KC_DEL , KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_END , KC_INS , KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Wild
  * ,-----------------------------------------------------------------------------------.
@@ -163,7 +163,7 @@ ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L
         KC_TRNS, CAD    , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, ALTINS , KC_TRNS, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, CSE    , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* NUM
  * ,-----------------------------------------------------------------------------------.
@@ -180,7 +180,7 @@ ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* SETTINGS
@@ -198,7 +198,7 @@ ESC_CTL, KC_A,     KC_S,     KC_D,    KC_F,  KC_G,   KC_H,  KC_J,  KC_K,    KC_L
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SP_DV  , SP_DVANS,SP_QW  , KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 )
 };
 
