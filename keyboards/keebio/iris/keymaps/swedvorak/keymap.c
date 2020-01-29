20f94c98f73eba7639adf49cc266670eadcc03db
MGrass1 <marcus.grass@gmail.com>
2020-01-29 17:09:13 +0100

    v002

--- keyboards/keebio/iris/keymaps/swedvorak/keymap.c
+++ keyboards/keebio/iris/keymaps/swedvorak/keymap.c
@@ -3,6 +3,8 @@
 
 #define NO_AA KC_LBRC          // å
 #define NO_ASTR LSFT(KC_BSLS)  // *
+#define NO_UDSC LSFT(NO_MINS)  // _
+#define AN_UDSC LSFT(KC_MINS)  // _
 
 enum {
     _DVORAK      = 0, // Dvorak for Swedish QWERTY Layout, can type åäö
@@ -46,16 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TAB  , SFTAPSTR, COMLBR  , DOTRBR , KC_P    , KC_Y    ,                             KC_F    , KC_G    ,  KC_C   , KC_R    , KC_L    , KC_BSPC, \
                 ESC_CTL , KC_A    , KC_O    , KC_E   , KC_U    , KC_I    ,                             KC_D    , KC_H    ,  KC_T   , KC_N    , KC_S    , KC_ENT , \
                 KC_LSFT , SCOLCOL , KC_Q    , KC_J   , KC_K    , KC_X    ,                             KC_B    , KC_M    ,  KC_W   , KC_V    , KC_Z    , KC_LSFT, \
-                KC_LCTL , KC_LGUI , KC_TRNS , KC_TRNS, LOWER   , KC_LALT , LOWER   ,         NUM     , KC_ALGR , RAISE   ,  KC_TRNS, KC_TRNS , SETTS   , KC_RCTL, \
-                                                       KC_TRNS , WILD    , KC_SPC  ,         KC_LSFT , RAISE   , KC_TRNS \
+                KC_LCTL , KC_LGUI , KC_LALT , WILD   , LOWER   , KC_SPC  , KC_LALT ,         KC_ALGR , KC_SPC  , NUM     ,  RAISE  , KC_ALGR , SETTS   , KC_RCTL, \
+                                                       KC_TRNS , NO_UDSC , KC_LSFT ,         KC_LSFT , KC_BSPC , KC_TRNS \
         ),
 
         [_DVORAK_ANSI] = LAYOUT( \
                 KC_TAB  , KC_QUOT , KC_COMM , KC_DOT , KC_P    , KC_Y    ,                             KC_F    , KC_G    ,  KC_C   , KC_R    , KC_L    , KC_BSPC, \
                 ESC_CTL , KC_A    , KC_O    , KC_E   , KC_U    , KC_I    ,                             KC_D    , KC_H    ,  KC_T   , KC_N    , KC_S    , KC_ENT , \
                 KC_LSFT , KC_SCLN , KC_Q    , KC_J   , KC_K    , KC_X    ,                             KC_B    , KC_M    ,  KC_W   , KC_V    , KC_Z    , KC_LSFT, \
-                KC_LCTL , KC_LGUI , KC_TRNS , KC_TRNS, LOWER   , KC_LALT , LOWER   ,         NUM     , KC_ALGR , RAISE   ,  KC_TRNS, KC_TRNS , SETTS   , KC_RCTL, \
-                                                       KC_TRNS , WILD    , KC_SPC  ,         KC_LSFT , RAISE   , KC_TRNS \
+                KC_LCTL , KC_LGUI , KC_LALT , WILD   , LOWER   , KC_SPC  , KC_LALT ,         KC_ALGR , KC_SPC  , NUM     ,  RAISE  , KC_ALGR , SETTS   , KC_RCTL, \
+                                                       KC_TRNS , AN_UDSC , KC_LSFT ,         KC_LSFT , KC_BSPC , KC_TRNS \
         ),
 
 
@@ -63,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TAB  , KC_Q    , KC_W    , KC_E   , KC_R    , KC_T    ,                             KC_Y    , KC_U    ,  KC_I   , KC_O    , KC_P    , KC_BSPC, \
                 ESC_CTL , KC_A    , KC_S    , KC_D   , KC_F    , KC_G    ,                             KC_H    , KC_J    ,  KC_K   , KC_L    , KC_SCLN , KC_ENT , \
                 KC_LSFT , KC_Z    , KC_X    , KC_C   , KC_V    , KC_B    ,                             KC_N    , KC_M    ,  KC_COMM, KC_DOT  , KC_QUOT , KC_LSFT, \
-                KC_LCTL , KC_LGUI , KC_TRNS , KC_TRNS, LOWER   , KC_LALT , LOWER   ,         NUM     , KC_ALGR , RAISE   ,  KC_TRNS, KC_TRNS , SETTS   , KC_RCTL, \
-                                                       KC_TRNS , WILD    , KC_SPC  ,         KC_LSFT , RAISE   , KC_TRNS \
+                KC_LCTL , KC_LGUI , KC_LALT , WILD   , LOWER   , KC_SPC  , KC_LALT ,         KC_ALGR , KC_SPC  , NUM     ,  RAISE  , KC_ALGR , SETTS   , KC_RCTL, \
+                                                       KC_TRNS , AN_UDSC , KC_LSFT ,         KC_LSFT , KC_BSPC , KC_TRNS \
         ),
 
         [_LOWER] = LAYOUT( \
