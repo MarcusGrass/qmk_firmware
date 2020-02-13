d5c745d3d04d1fef6531d2e1d68c4ba9bc6cbc4c
MGrass1 <marcus.grass@gmail.com>
2020-02-13 18:59:16 +0100

    rem kc

--- keyboards/keebio/iris/keymaps/swedvorak/keymap.c
+++ keyboards/keebio/iris/keymaps/swedvorak/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 ESC_CTL , KC_T    , KC_Q    , KC_W   , KC_E    , KC_R    ,                             KC_H    , KC_J    ,  KC_K   , KC_L    , KC_SCLN , KC_ENT , \
                 KC_LSFT , KC_B    , KC_A    , KC_S   , KC_D    , KC_F    ,                             KC_N    , KC_M    ,  KC_COMM, KC_DOT  , KC_QUOT , KC_LSFT, \
                 KC_LCTL , KC_Z    , KC_X    , KC_C   , KC_V    , KC_B    , KC_M   ,          KC_ALGR , KC_SPC  , NUM     ,  RAISE  , KC_ALGR , SETTS   , KC_RCTL, \
-                                                       KC_LGUI , KC_LALT , KC_SPC ,          KC_LSFT , KC_BSPC , KC_LOWER \
+                                                       KC_LGUI , KC_LALT , KC_SPC ,          KC_LSFT , KC_BSPC , LOWER \
         ),
 
         [_LOWER] = LAYOUT( \
